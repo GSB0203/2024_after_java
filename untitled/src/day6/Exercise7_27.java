4b197d34dce13475d2c00e95d17d91c9f76c70e6
gimseungbin0203 <gimseungbin0203@gmail.com>
2024-05-07 17:20:19 +0900

    Day 6

---
+++ untitled/src/day6/Exercise7_27.java
@@ -0,0 +1,25 @@
+package untitled.src.day6;
+
+class Outer3 {
+  int value = 10;
+
+  class Inner {
+    int value = 20;
+    void method1() {
+      int value = 30;
+
+      System.out.println(value);
+      System.out.println(Inner.this.value);
+      System.out.println(Outer3.this.value);
+    }
+  }
+}
+
+public class Exercise7_27 {
+  public static void main(String[] args) {
+    Outer3 outer3 = new Outer3();
+    Outer3.Inner inner = outer3.new Inner();
+
+    inner.method1();
+  }
+}
